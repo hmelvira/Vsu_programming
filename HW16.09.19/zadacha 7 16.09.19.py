b9ae082858759fedf5caa7caffa93f194efc551d
hmelvira <49476910+hmelvira@users.noreply.github.com>
2019-11-04 10:29:19 +0300

    Update zadacha 7 16.09.19.py

--- HW16.09.19/zadacha 7 16.09.19.py
+++ HW16.09.19/zadacha 7 16.09.19.py
@@ -1,5 +1,8 @@
-n = float(input()) 
-if n == round(n): 
-    print ('Целое') 
-else: 
-    print ('Не целое')
+x, y = map(int,input().split()) 
+s = 0 
+if x > y: 
+    x, y = y, x 
+for i in range(x,y+1,1): 
+    if i % 5 == 0: 
+        s += 1 
+print (s)
