68d187f7c5f9d3f05c2dc5ca2305a2a465adb7a5
hmelvira <49476910+hmelvira@users.noreply.github.com>
2019-11-04 10:55:42 +0300

    Create hw4.py

---
+++ hw4.py
@@ -0,0 +1,14 @@
+def combsort(data):
+    gap = len(data) # разрыв
+    o = True # обмен
+    while gap > 1 or o:
+        gap = max(1, int(gap / 1.25))
+        o = False
+        for i in range(len(data) - gap):
+            j = i + gap
+            if data[i] > data[j]:
+                data[i], data[j] = data[j], data[i] 
+                o = True 
+    return data 
+a = list(map(int,input().split())) 
+print (combsort(a)) 
