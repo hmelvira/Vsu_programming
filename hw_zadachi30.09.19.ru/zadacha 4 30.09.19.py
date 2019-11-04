7e98ae698d5e737e84e38753b7276f6822738b5d
hmelvira <49476910+hmelvira@users.noreply.github.com>
2019-11-04 10:42:43 +0300

    Update zadacha 4 30.09.19.py

--- hw_zadachi30.09.19.ru/zadacha 4 30.09.19.py
+++ hw_zadachi30.09.19.ru/zadacha 4 30.09.19.py
@@ -5,6 +5,7 @@ while len(n) > 0:
         m.append ([n[0], n.count(n[0])]) 
         x = n[0] 
         while n.count(x) > 0: 
-          n.remove(x) 
+            n.remove(x) 
         else: 
-             break 
+            break 
+            print ('\n'.join(m[i][0]+' | '+str(m[i][1]) for i in range (0,len(m))))
