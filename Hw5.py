99c5eca2d3a987a8682dcd1f80c0aa4861d9a882
hmelvira <49476910+hmelvira@users.noreply.github.com>
2019-11-04 12:16:38 +0300

    Create Hw5.py

---
+++ Hw5.py
@@ -0,0 +1,31 @@
+graph = { 
+    1: [2, 3, 8], 
+    2: [1, 7, 9], 
+    3: [1, 4, 10], 
+    4: [3, 6], 
+    5: [6, 10], 
+    6: [4, 5], 
+    7: [2, 8, 9], 
+    8: [1, 10], 
+    9: [2, 6, 7], 
+    10: [3, 5, 8] 
+} 
+
+visited = set() 
+q = [] 
+s = [] 
+
+def bfs(a): 
+    if a in visited: 
+        return 
+    visited.add(a) 
+    s.append(a) 
+    for i in graph[a]: 
+        if not i in visited: 
+            q.append(i) 
+    while q: 
+        bfs(q.pop(0)) 
+
+start = int(input()) 
+bfs(start) 
+print(s)
