75ebccc9fc8060b61dd6fb02b9307c8afa63d3c1
hmelvira <49476910+hmelvira@users.noreply.github.com>
2019-11-06 22:35:42 +0300

    Update heshtabl.py

--- heshtabl.py
+++ heshtabl.py
@@ -1,12 +1,25 @@
-storage = [['Null']*19 for x in range(172)]
+storage = [['Null']*31 for x in range(2049)]
 
 def hash(key):
     s = 0
-    for i in range(len(key)-1):
-        s += ord(key[i]) - 65
-    s -= ord(key[len(key)-1]) - 65
-    ind = (''.join(format(ord(x), 'b') for x in key)).count('0')
-    return s, ind
+    index2 = (''.join(format(ord(x), 'b') for x in key)).count('0') # сумма 0 в 2-м представлении ключа
+    index = ''.join('0'+format(ord(x), 'b') for x in key)
+    for i in range(2):
+        index = linetokey(index)
+    index = int(index, 2) » 1 # перевод из 2 системы в 10 и побитовый сдвиг вправо для уменьшения значения
+    return index, index2
+
+def linetokey(line):
+    part1, part2 = map(''.join, zip(*[iter(line)]*(len(line)//2)))
+    key = ''
+    if part1 == part2:
+        key = part1 # т.к. XOR выдаст 000...0
+    else:
+        lpart1 = list(map(int,part1))
+        lpart2 = list(map(int,part2))
+        for i in range(len(lpart1)):
+            key += str(lpart1[i] ^ lpart2[i]) # XOR
+    return key
 
 def get_value(key):
     index, index2 = hash(key)
@@ -17,9 +30,8 @@ def set_value(key, value):
     if storage[index][index2] == 'Null':
         storage[index][index2] = value
     else:
-        previous = storage[index][index2]
+        print('Значение '+str(storage[index][index2])+' заменено на значение '+str(value))
         storage[index][index2] = value
-        print('Значение '+str(previous)+' заменено на значение '+str(value))
 
 def del_value(key):
     index, index2 = hash(key)
@@ -30,14 +42,14 @@ def del_value(key):
 while True:
     command = input('Read, Write, Delete, Exit (r/w/d/e): ')
     if command == 'w':
-        key = input('Ключ (состоит из 3 букв): ')
-        value = input('Значение: ')
+        key = input('Введите ключ (максимум 6 символов): ')
+        value = input('Введите значение: ')
         set_value(key, value)
     if command == 'r':
-        key = input('Ключ (состоит из 3 букв): ')
-        print(get_value(key))
+        key = input('Введите ключ (максимум 6 символов): ')
+        print('Значение: '+str(get_value(key)))
     if command == 'd':
-        key = input('Ключ (состоит из 3 букв): ')
+        key = input('Введите ключ (максимум 6 символов): ')
         print('Удалено значение: '+del_value(key))
     if command == 'e':
-        break
+        break 
